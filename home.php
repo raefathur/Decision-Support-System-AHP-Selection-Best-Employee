8eec6e1308af5a130ac6f76ea92a91a94b7f6d34
raefathur <rayhanfatur2311@gmail.com>
2024-03-24 12:24:41 +0700

    first commit

---
+++ home.php
@@ -0,0 +1,21 @@
+<div class="page-header">
+    <h2>Sistem Penunjang Keputusan Pemilihan Pegawai Terbaik di PT Mitra Tiga Putera</h2>
+    Sistem ini membantu pengambil keputusan dalam menentukan pegawai terbaik dengan membandingkan setiap kandidat pegawai pada setiap kriteria,
+    metode yang digunakan adalah AHP (<i>Analytical Hierarchy Process</i>).<br /><br />
+
+    <b>Proses yang Anda harus lakukan :</b>
+    <ol>
+        <li>Masukkan Kriteria yang dibutuhkan.</li>
+        <li>Masukkan Sub Kriteria dari setiap kriteria yang dibutuhkan.</li>
+        <li>Masukkan Alternatif/Pegawai.</li>
+        <li>Mulai seleksi dengan metode AHP :
+            <ul>
+                <li>Masuk ke Nilai Bobot Kriteria dan isikan nilai perbandingan kriteria untuk menentukan bobot kriteria.</li>
+                <li>Masuk ke Nilai Bobot SubKriteria dan isikan nilai perbandingannya.</li>
+                <li>Masuk ke Nilai Bobot Alternatif dan isikan bobot alternatifnya.</li>
+                <li>Masuk ke Perhitungan untuk melihat hasil perhitungan dan perangkingan.</li>
+            </ul>
+        </li>
+        <li>Anda juga dapat mencetak data Kriteria, Sub Kriteria, Alternatif dan Rangking.</li>
+    </ol>
+</div>
